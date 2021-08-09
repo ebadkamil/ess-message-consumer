2c80bf80ebb0a3502dfb11355f9ce08e4923fb68
Ebad Kamil <ebad.kamil@ess.eu>
2021-08-09 14:53:41 +0200

    Add DeserializerFactory class

---
+++ ess_message_consumer/deserializer.py
@@ -0,0 +1,70 @@
+from streaming_data_types import (
+    deserialise_6s4t,
+    deserialise_answ,
+    deserialise_ev42,
+    deserialise_f142,
+    deserialise_hs00,
+    deserialise_pl72,
+    deserialise_wrdn,
+    deserialise_x5f2,
+)
+
+
+class StatusMessage:
+    def deserialize(self, message):
+        return deserialise_x5f2(message)
+
+
+class FwResponseMessage:
+    def deserialize(self, message):
+        return deserialise_answ(message)
+
+
+class FwFinishedMessage:
+    def deserialize(self, message):
+        return deserialise_wrdn(message)
+
+
+class RunStopMessage:
+    def deserialize(self, message):
+        return deserialise_6s4t(message)
+
+
+class RunStartMessage:
+    def deserialize(self, message):
+        return deserialise_pl72(message)
+
+
+class LogMessage:
+    def deserialize(self, message):
+        return deserialise_f142(message)
+
+
+class EventMessage:
+    def deserialize(self, message):
+        return deserialise_ev42(message)
+
+
+class HistogramMessage:
+    def deserialize(self, message):
+        return deserialise_hs00(message)
+
+
+class DeserializerFactory:
+    _message_handler = {
+        b"x5f2": StatusMessage,
+        b"answ": FwResponseMessage,
+        b"wrdn": FwFinishedMessage,
+        b"6s4t": RunStopMessage,
+        b"pl72": RunStartMessage,
+        b"f142": LogMessage,
+        b"ev42": EventMessage,
+        b"hs00": HistogramMessage,
+    }
+
+    @classmethod
+    def from_serialized_type(cls, type):
+        if type in cls._message_handler:
+            return cls._message_handler[type]()
+
+        raise NotImplementedError(f"Unspecified serialization of type: {type}")
