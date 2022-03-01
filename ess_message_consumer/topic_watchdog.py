28957c1cd2eb14b3e3cdc61d7c25eb5e37b8016c
Ebad Kamil <ebad.kamil@ess.eu>
2022-03-01 12:22:27 +0100

    Check thread alive status and cleanly stop

--- ess_message_consumer/topic_watchdog.py
+++ ess_message_consumer/topic_watchdog.py
@@ -1,16 +1,16 @@
 import time
 import uuid
 from logging import Logger
+from threading import Thread
 
 from confluent_kafka import Consumer
-
-from ess_message_consumer.utils import run_in_thread
 
 
 class TopicWatchDog:
     def __init__(self, broker: str, logger: Logger = None):
         self._broker = broker
         self._logger = logger.error if logger else print
+        self._stop = False
         self._existing_topics = set()
 
         try:
@@ -24,10 +24,13 @@ class TopicWatchDog:
             msg = f"Unable to create consumers: {error}"
             self._logger(msg)
             raise
+        self._watch_thread = Thread(target=self._update_exisiting_topics)
 
-    @run_in_thread
     def track_topics(self):
-        while True:
+        self._watch_thread.start()
+
+    def _update_exisiting_topics(self):
+        while not self._stop:
             existing_topics = self.get_available_topics()
             self._existing_topics.update(existing_topics)
             time.sleep(5)
@@ -38,3 +41,8 @@ class TopicWatchDog:
     @property
     def existing_topics(self):
         return self._existing_topics
+
+    def close(self):
+        self._stop = True
+        if self._watch_thread and self._watch_thread.is_alive():
+            self._watch_thread.join()
